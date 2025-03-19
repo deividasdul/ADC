1ea3f49219e0c3d5fc0bb4dc19c37dec218e861b
deividasdul <deividas.dul@gmail.com>
2025-03-19 16:18:59 +0200

    add header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,7 +1,23 @@
+import { AppBar, Toolbar } from "@mui/material";
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <AppBar position="relative">
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "flex-end",
+          gap: 2,
+        }}
+      >
+        <Link href={"/"}>Home</Link>
+        <Link href={"/login"}>Login</Link>
+        <Link href={"/register"}>Register</Link>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
