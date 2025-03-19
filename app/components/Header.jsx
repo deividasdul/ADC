52c40b2c2e945f892283db1824496aef20b7d429
deividasdul <deividas.dul@gmail.com>
2025-03-19 16:32:51 +0200

    add dogshit tailwind css MUI material is better btw

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,19 +4,30 @@ import React from "react";
 
 const Header = () => {
   return (
-    <AppBar position="relative">
-      <Toolbar
-        sx={{
-          display: "flex",
-          justifyContent: "flex-end",
-          gap: 2,
-        }}
-      >
-        <Link href={"/"}>Home</Link>
-        <Link href={"/login"}>Login</Link>
-        <Link href={"/register"}>Register</Link>
-      </Toolbar>
-    </AppBar>
+    // <AppBar position="relative">
+    //   <Toolbar
+    //     sx={{
+    //       display: "flex",
+    //       justifyContent: "flex-end",
+    //       gap: 2,
+    //     }}
+    //   >
+    //     <Link href={"/"}>Home</Link>
+    //     <Link href={"/login"}>Login</Link>
+    //     <Link href={"/register"}>Register</Link>
+    //   </Toolbar>
+    // </AppBar>
+    <nav className="flex relative justify-end gap-4 text-3xl p-4 bg-gray-200 text-black">
+      <a className="hover:underline hover:scale-115" href="/">
+        Home
+      </a>
+      <a className="hover:underline hover:scale-115" href="/login">
+        Login
+      </a>
+      <a className="hover:underline hover:scale-115" href="/register">
+        Register
+      </a>
+    </nav>
   );
 };
 
