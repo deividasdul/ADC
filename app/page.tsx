d85aa428e71937483e3bbd6f1aed0deb2c50f0f3
admin <augustaspauzas@gmail.com>
2025-03-19 15:35:36 +0200

    drawio db

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <p> ALIO VALIO  </p>
+        <p> ALIO VALIO test 333  </p>
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
