ee46425b313c6c7e48f24960d609d1e8b3c56762
deividasdul <deividas.dul@gmail.com>
2025-03-20 18:02:58 +0200

    xdddddddd

--- app/page.tsx
+++ app/page.tsx
@@ -1,108 +1,92 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import React from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+
+const LetterA = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Amiggo</h1>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <p>migga 30</p>
-        <p> ALIO VALIO</p>
-        <p> ALIO VALIO test 333 </p>
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-      asd asd asd asd
-    </div>
+    <>
+      <mesh position={[-2, 0, -10]} rotation={[0, 0, -0.45]}>
+        <boxGeometry args={[1, 5, 1]} />
+        <meshPhysicalMaterial roughness={0} color={"#b6b6b6"} />
+      </mesh>
+      <mesh position={[0, 0, -10]} rotation={[0, 0, 0.45]}>
+        <boxGeometry args={[1, 5, 1]} />
+        <meshPhysicalMaterial roughness={0} color={"#b6b6b6"} />
+      </mesh>
+      <mesh position={[-1, -1, -10]} rotation={[0, 0, Math.PI / 2]}>
+        <boxGeometry args={[1, 5, 1]} />
+        <meshPhysicalMaterial roughness={0} color={"#b6b6b6"} />
+      </mesh>
+    </>
   );
-}
+};
+
+const LetterD = () => {
+  return (
+    <>
+      <mesh position={[3, 0, -10]} rotation={[0, 0, 0]}>
+        <boxGeometry args={[1, 5, 1]} />
+        <meshPhysicalMaterial roughness={0} color={"#FF0000"} />
+      </mesh>
+      <mesh position={[3, 0, -10]} rotation={[0, 0, -Math.PI / 2]} scale={0.2}>
+        <torusGeometry args={[10, 2, 16, 10, 3]} />
+        <meshPhysicalMaterial roughness={0} color={"#FF0000"} />
+      </mesh>
+    </>
+  );
+};
+
+const LetterC = () => {
+  return (
+    <mesh position={[9, 0, -10]} rotation={[0, 0, Math.PI / 2]} scale={0.2}>
+      <torusGeometry args={[10, 2, 16, 10, 3]} />
+      <meshPhysicalMaterial roughness={0} color={"#DB5B5B"} />
+    </mesh>
+  );
+};
+
+const RotatingCamera = ({ children }) => {
+  const myCamera = React.useRef();
+  let angle = React.useRef(0);
+
+  useFrame(() => {
+    angle.current += 0.01; // Adjust speed
+    myCamera.current.position.x = 5 * Math.cos(angle.current);
+    myCamera.current.position.z = 5 * Math.sin(angle.current);
+    myCamera.current.lookAt(0, 0, -5); // Always look at the origin
+  });
+
+  return (
+    <PerspectiveCamera ref={myCamera} position={[0, 0, -500]}>
+      {children}
+    </PerspectiveCamera>
+  );
+};
+
+const Home = () => {
+  return (
+    <>
+      <Canvas
+        style={{
+          width: "100vw",
+          height: "50vh",
+        }}
+      >
+        <ambientLight color={"black"} intensity={0.75} />
+        <directionalLight position={[-5, 5, 50]} intensity={1.5} />
+        {/* <PerspectiveCamera position={[0, 0, 5]}> */}
+        <RotatingCamera>
+          <LetterA />
+          <LetterD />
+          <LetterC />
+        </RotatingCamera>
+        {/* </PerspectiveCamera> */}
+        <OrbitControls />
+      </Canvas>
+    </>
+  );
+};
+
+export default Home;
