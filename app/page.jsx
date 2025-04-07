b627d6a83f2b1e342977bb6d1ffcecc72b58d331
deividasdul <deividas.dul@gmail.com>
2025-04-07 16:46:06 +0300

    New homepage

--- app/page.jsx
+++ app/page.jsx
@@ -7,10 +7,18 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-const Section = ({ ref, label }) => {
+import starVertexShader from "./shaders/stars/vertex";
+import starFragmentShader from "./shaders/stars/fragment";
+
+const Section = ({ ref, label, paragraph, pRef }) => {
   return (
-    <div ref={ref} className="h-screen text-7xl m-16 font-bold text-green-500">
-      <h2>{label}</h2>
+    <div ref={ref} className="h-screen  m-16 ">
+      <h2 className="text-7xl font-bold text-green-500 mb-4 underline">
+        {label}
+      </h2>
+      <p className="w-1/2  text-2xl" ref={pRef}>
+        {paragraph}
+      </p>
     </div>
   );
 };
@@ -21,7 +29,16 @@ const page = () => {
       width: window.innerWidth,
       height: window.innerHeight,
     };
+    const mouse = {
+      x: 0,
+      y: 0,
+    };
     const canvas = document.querySelector(".webgl");
+
+    window.addEventListener("mousemove", (event) => {
+      mouse.x = event.clientX / sizes.width;
+      mouse.y = 1 - (event.clientY / sizes.height) * 2;
+    });
 
     window.addEventListener("resize", () => {
       sizes.width = window.innerWidth;
@@ -38,6 +55,9 @@ const page = () => {
     const scene = new THREE.Scene();
 
     // Camera
+    const cameraGroup = new THREE.Group();
+    scene.add(cameraGroup);
+
     const camera = new THREE.PerspectiveCamera(
       50,
       sizes.width / sizes.height,
@@ -46,36 +66,7 @@ const page = () => {
     );
     camera.position.z = 5;
 
-    scene.add(camera);
-
-    // Stars
-
-    const starsGeometry = new THREE.BufferGeometry();
-    const count = 10000;
-
-    const vertices = new Float32Array(count * 3);
-
-    for (let i = 0; i < count; i++) {
-      let i3 = i * 3;
-
-      vertices[i3 + 0] = (0.5 - Math.random()) * 10;
-      vertices[i3 + 1] = (0.5 - Math.random()) * 30;
-      vertices[i3 + 2] = (0.5 - Math.random()) * 5;
-    }
-
-    starsGeometry.setAttribute(
-      "position",
-      new THREE.BufferAttribute(vertices, 3)
-    );
-
-    const starsMaterial = new THREE.PointsMaterial({
-      color: "red",
-      size: 0.02,
-      sizeAttenuation: true,
-    });
-
-    const stars = new THREE.Points(starsGeometry, starsMaterial);
-    scene.add(stars);
+    cameraGroup.add(camera);
 
     // Renderer
     const renderer = new THREE.WebGLRenderer({
@@ -86,8 +77,67 @@ const page = () => {
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
     renderer.setSize(sizes.width, sizes.height);
 
+    // Stars
+    const starsGeometry = new THREE.BufferGeometry();
+    const count = 1000;
+
+    const vertices = new Float32Array(count * 3);
+    const colors = new Float32Array(count * 3);
+    const randomness = new Float32Array(count);
+
+    for (let i = 0; i < count; i++) {
+      let i3 = i * 3;
+
+      vertices[i3 + 0] = (0.5 - Math.random()) * 12.5;
+      vertices[i3 + 1] = (0.5 - Math.random()) * 5 * 3;
+      vertices[i3 + 2] = (0.5 - Math.random()) * 5;
+
+      colors[i + 0] = Math.random();
+      colors[i3 + 1] = Math.random();
+      colors[i3 + 2] = Math.random();
+
+      randomness[i] = Math.random() * 5;
+    }
+
+    starsGeometry.setAttribute(
+      "position",
+      new THREE.BufferAttribute(vertices, 3)
+    );
+    starsGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
+    starsGeometry.setAttribute(
+      "aRandom",
+      new THREE.BufferAttribute(randomness, 1)
+    );
+
+    const starsMaterial = new THREE.ShaderMaterial({
+      uniforms: {
+        uTime: { value: 0 },
+        uSize: { value: 100.0 * renderer.getPixelRatio() },
+      },
+      vertexShader: starVertexShader,
+      vertexColors: true,
+      transparent: true,
+      fragmentShader: starFragmentShader,
+      blending: THREE.AdditiveBlending,
+      depthWrite: false,
+    });
+
+    const stars = new THREE.Points(starsGeometry, starsMaterial);
+    scene.add(stars);
+
     // Animation
+    const clock = new THREE.Clock();
+
     const tick = () => {
+      const elapsedTime = clock.getElapsedTime();
+
+      starsMaterial.uniforms.uTime.value = elapsedTime;
+
+      cameraGroup.position.y = -(scrollY / sizes.height) * 1.5;
+
+      camera.position.x = mouse.x;
+      camera.position.y = mouse.y;
+
       renderer.render(scene, camera);
 
       window.requestAnimationFrame(tick);
@@ -100,37 +150,101 @@ const page = () => {
   const secondRef = useRef();
   const thirdRef = useRef();
 
+  const firstParagraphRef = useRef();
+  const secondParagraphRef = useRef();
+  const thirdParagraphRef = useRef();
+
   gsap.registerPlugin(ScrollTrigger);
 
   useGSAP(() => {
-    gsap.from(firstRef.current, {
-      x: "-100vw",
-      duration: 0.75,
-      ease: "sine.inOut",
-    });
+    const timelineOne = gsap.timeline();
 
-    gsap.from(secondRef.current, {
-      x: "-100vw",
-      duration: 0.75,
-      ease: "sine.inOut",
+    timelineOne
+      .from(firstRef.current, {
+        x: "-100vw",
+        duration: 0.75,
+        ease: "sine.inOut",
+      })
+      .from(
+        firstParagraphRef.current,
+        {
+          duration: 0.75,
+          x: "-100vw",
+          ease: "sine.inOut",
+        },
+        0
+      );
+
+    const timelineTwo = gsap.timeline({
       scrollTrigger: secondRef.current,
     });
 
-    gsap.from(thirdRef.current, {
-      x: "-100vw",
-      duration: 0.75,
-      ease: "sine.inOut",
+    timelineTwo
+      .from(secondRef.current, {
+        x: "-100vw",
+        duration: 0.75,
+        ease: "sine.inOut",
+      })
+      .from(
+        secondParagraphRef.current,
+        {
+          duration: 0.75,
+          x: "-100vw",
+          ease: "sine.inOut",
+        },
+        0.1
+      );
+
+    // Third
+    const timelineThree = gsap.timeline({
       scrollTrigger: thirdRef.current,
     });
+
+    timelineThree
+      .from(thirdRef.current, {
+        x: "-100vw",
+        duration: 0.75,
+        ease: "sine.inOut",
+      })
+      .from(
+        thirdParagraphRef.current,
+        {
+          duration: 0.75,
+          x: "-100vw",
+          ease: "sine.inOut",
+        },
+        0
+      );
   });
 
   return (
     <>
-      <canvas className="webgl fixed top-0 left-0 w-full h-full"></canvas>
+      <canvas className="webgl fixed top-30 left-0 w-full h-full"></canvas>
 
-      <Section ref={firstRef} label={"CHAT ANONYMOUSLY"} />
-      <Section ref={secondRef} label={"USE FREE OF CHARGE"} />
-      <Section ref={thirdRef} label={"STAY IN TOUCH"} />
+      <Section
+        ref={firstRef}
+        pRef={firstParagraphRef}
+        label={"CHAT ANONYMOUSLY"}
+        paragraph={
+          "Connect and converse without revealing your identity. Whether you're seeking advice, sharing ideas, or simply enjoying a friendly chat, our platform allows you to interact freely while keeping your personal information private. Experience secure and anonymous communication that puts your privacy first."
+        }
+      />
+      <Section
+        ref={secondRef}
+        pRef={secondParagraphRef}
+        label={"USE FREE OF CHARGE"}
+        paragraph={
+          "Access all of our services completely free of charge. No hidden fees, no subscriptions — just straightforward, no-cost access to a secure and anonymous communication platform. Start using it today, and enjoy hassle-free chatting without worrying about costs."
+        }
+      />
+      <Section
+        ref={thirdRef}
+        pRef={thirdParagraphRef}
+        label={"STAY IN TOUCH"}
+        paragraph={
+          "Never miss a moment. Stay connected with friends, family, or colleagues effortlessly. Whether it's through regular updates or instant messages, our platform makes it easy to maintain meaningful connections. Keep the conversation going, anytime, anywhere."
+        }
+      />
     </>
   );
 };
--- app/shaders/stars/vertex.js
+++ app/shaders/stars/vertex.js
@@ -1,7 +1,27 @@
-const vertexShader = `
+const starVertexShader = `
+    uniform float uSize;
+    uniform float uTime;
+
+    attribute float aRandom;
+
+
+    varying vec3 vColor; 
+
     void main() {
-        Hello, World
+        vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+        modelPosition.y += sin(uTime * 0.05) * modelPosition.z;
+        modelPosition.x += cos(uTime * 0.05) * modelPosition.y; 
+
+        vec4 viewPosition = viewMatrix * modelPosition;
+        vec4 projectedPosition = projectionMatrix * viewPosition;
+
+        gl_Position = projectedPosition;
+        gl_PointSize = uSize * aRandom;
+        gl_PointSize *= (1.0 / - viewPosition.z); 
+
+        vColor = color;
     }
 `;
 
-export default vertexShader;
+export default starVertexShader;
