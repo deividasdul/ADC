4293630c1dd03103559ce21bcb5d866df71fdc55
admin <augustaspauzas@gmail.com>
2025-04-23 17:09:47 +0300

    farewell?

---
+++ app/farewell/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+const farewell = () => {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-200">
+        <p className="text-5xl px-24">Due to lack of funding the ADC project has been discontinued </p>
+
+
+      </div>
+    </>
+  );
+};
+
+export default farewell;
